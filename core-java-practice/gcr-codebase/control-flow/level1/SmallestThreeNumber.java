7aaacdd928f9e6ede41c7f9a6b7375262cc6714a
Sadgi-tiwari <sadgitiwari221@gmail.com>
2026-06-09 20:13:29 +0530

    Added core-java-practice

---
+++ core-java-practice/gcr-codebase/control-flow/level1/SmallestThreeNumber.java
@@ -0,0 +1,22 @@
+import java .util.Scanner;
+public class SmallestThreeNumber {
+    public static void main(String[]args)  {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        int smallest = num1;
+
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+
+        System.out.println("The smallest number is: " + smallest);
+    }
