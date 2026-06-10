509403542cf8f1dd3cd922de89629d5850bb259f
Sadgi-tiwari <sadgitiwari221@gmail.com>
2026-06-10 18:59:30 +0530

    Added programming-element

---
+++ core-java-practice/gcr-codebase/programming-element/level1/KilometerToMiles.java
@@ -0,0 +1,15 @@
+
+
+public class KilometerToMiles {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in kilometers: ");
+        double kilometers = sc.nextDouble();
+
+        double miles = kilometers / 1.60934;
+
+        System.out.println("The distance in miles is " + miles);
+    }
+}
