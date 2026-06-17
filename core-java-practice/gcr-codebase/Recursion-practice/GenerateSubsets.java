0fdce1cff0d963a065ff045c363d059890b39fb6
Sadgi-tiwari <sadgitiwari221@gmail.com>
2026-06-17 23:07:10 +0530

    Added Recursion-practice

---
+++ core-java-practice/gcr-codebase/Recursion-practice/GenerateSubsets.java
@@ -0,0 +1,23 @@
+public class GenerateSubsets {
+
+    static void generateSubsets(int[] arr, int index, String subset) {
+        if (index == arr.length) {
+            System.out.println("[" + subset + "]");
+            return;
+        }
+
+        generateSubsets(arr, index + 1, subset);
+
+        if (subset.isEmpty()) {
+            generateSubsets(arr, index + 1, String.valueOf(arr[index]));
+        } else {
+            generateSubsets(arr, index + 1, subset + "," + arr[index]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2};
+
+        generateSubsets(arr, 0, "");
+    }
+}
